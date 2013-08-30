a6a366b71779834e12e931d510c9d24ad6b2ed40
Adam Mazur <mazik.wyry@gmail.com>
2013-08-30 12:25:16 +0200

    Responsive fixes

--- js/js.js
+++ js/js.js
@@ -1,38 +1,83 @@
-	$(".top-buttons").hover(function(){
-		$(this).children("img").stop(true,true).transition({
-		  rotate: "20deg"
-		});
-		$(this).children(".button-desc").stop(true,true).transition({
-		  x: "3px"
-		});
-	},function(){
-		$(this).children("img").stop(true,true).transition({
-		  rotate: "0deg"
-		});
-		$(this).children(".button-desc").stop(true,true).transition({
-		  x: "0px"
-		});
-	});
+	var rotator_counter = 1;
+	var rotator_max = 3;
 
-	$(".group a").hover(function(){
-		$(this).stop(true,true).transition({
-		  x: "3px"
-		},200,"ease");
- 	},function(){
-		$(this).stop(true,true).transition({
-		  x: "0px"
-		},200,"ease");
-	});
+	$(document).ready(function(){
+		$("#rot1 .rotator_content").animate({
+		  opacity:"1.0",top:'0px'
+		},500);
+		var oldScroll = $(window).scrollTop();
+		setInterval(
+			function(){
+				if(rotator_counter==rotator_max){
+					$("#rot"+rotator_counter).animate({
+					  opacity:"0"
+					},1000);
+					$("#rot"+rotator_counter+" .rotator_content").animate({
+					  opacity:"0",top:'-10px'
+					},300);
+					$("#rot1").animate({
+					  opacity:"1.0"
+					},500);
+					$("#rot1 .rotator_content").animate({
+					  opacity:"1.0",top:'0px'
+					},500);
+					rotator_counter = 1;
+				}else{
+					$("#rot"+rotator_counter).animate({
+					  opacity:"0"
+					},1000);
+					$("#rot"+rotator_counter+" .rotator_content").animate({
+					  opacity:"0",top:'-10px'
+					},300);
 
-	$(".green_button").hover(function(){
-		$(this).stop(true,true).transition({
-		  scale: "1.08"
-		},400,"easeInOutBack");
- 	},function(){
-		$(this).stop(true,true).transition({
-		  scale: "1.0"
-		},100,"easeInOutBack");
-	});
+					$("#rot"+(rotator_counter+1)).animate({
+					  opacity:"1.0"
+					},500);
+					$("#rot"+(rotator_counter+1)+" .rotator_content").animate({
+					  opacity:"1.0",top:'0px'
+					},500);
+					rotator_counter++;
+				}
+			},8000
+		);
+
+		$(".epidemia_header").fitText(0.55, {maxFontSize: '77px' });
+		$(".sub_header").fitText(1.7, {maxFontSize: '22px' });
+
+		$(window).bind('scroll',function(e){ 
+		    var scrolled = $(window).scrollTop();
+		    // if(scrolled>85){
+		    // 	if(oldScroll>scrolled){
+		    // 		$("header").slideDown(200);
+		    // 	}else{
+		    // 		$("header").slideUp(200);
+		    // 	}
+		    // }else{
+		    // 	$("header").show();
+		    // }
+		    oldScroll = scrolled;
+		});
+
+	// 		$(".top-buttons").hover(function(){
+	// 	$(this).children(".button-desc").stop(true,true).transition({
+	// 	  x: "3px"
+	// 	});
+	// },function(){
+
+	// 	$(this).children(".button-desc").stop(true,true).transition({
+	// 	  x: "0px"
+	// 	});
+	// });
+
+	// $(".group a").hover(function(){
+	// 	$(this).stop(true,true).transition({
+	// 	  x: "3px"
+	// 	},200,"ease");
+ // 	},function(){
+	// 	$(this).stop(true,true).transition({
+	// 	  x: "0px"
+	// 	},200,"ease");
+	// });
 
 	$(".social_icon").hover(function(){
 		$(this).children("div").stop(true,true).animate({
@@ -104,65 +149,4 @@
 		}
 	});
 
-
-	var rotator_counter = 1;
-	var rotator_max = 3;
-
-	$(document).ready(function(){
-		$("#rot1 .rotator_content").animate({
-		  opacity:"1.0",top:'0px'
-		},500);
-		var oldScroll = $(window).scrollTop();
-		setInterval(
-			function(){
-				if(rotator_counter==rotator_max){
-					$("#rot"+rotator_counter).animate({
-					  opacity:"0"
-					},1000);
-					$("#rot"+rotator_counter+" .rotator_content").animate({
-					  opacity:"0",top:'-10px'
-					},300);
-					$("#rot1").animate({
-					  opacity:"1.0"
-					},500);
-					$("#rot1 .rotator_content").animate({
-					  opacity:"1.0",top:'0px'
-					},500);
-					rotator_counter = 1;
-				}else{
-					$("#rot"+rotator_counter).animate({
-					  opacity:"0"
-					},1000);
-					$("#rot"+rotator_counter+" .rotator_content").animate({
-					  opacity:"0",top:'-10px'
-					},300);
-
-					$("#rot"+(rotator_counter+1)).animate({
-					  opacity:"1.0"
-					},500);
-					$("#rot"+(rotator_counter+1)+" .rotator_content").animate({
-					  opacity:"1.0",top:'0px'
-					},500);
-					rotator_counter++;
-				}
-			},8000
-		);
-
-		$(".epidemia_header").fitText(0.55, {maxFontSize: '77px' });
-		$(".sub_header").fitText(1.7, {maxFontSize: '22px' });
-		$(".stats-numbers").fitText(1.0, {maxFontSize: '32px' });
-
-		$(window).bind('scroll',function(e){ 
-		    var scrolled = $(window).scrollTop();
-		    // if(scrolled>85){
-		    // 	if(oldScroll>scrolled){
-		    // 		$("header").slideDown(200);
-		    // 	}else{
-		    // 		$("header").slideUp(200);
-		    // 	}
-		    // }else{
-		    // 	$("header").show();
-		    // }
-		    oldScroll = scrolled;
-		});
 	});
